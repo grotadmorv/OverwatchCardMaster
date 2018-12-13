60fbaeb17237fa0381642a683767507ca2e5b7b3
Sahra Vadrot <apexp@hotmail.fr>
2018-12-13 15:07:04 +0100

    :sparkles: render image and press
    image onpress #10

--- App/Containers/GameBoardScreen.js
+++ App/Containers/GameBoardScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, Text, Image, Button } from 'react-native'
+import { View, Text, Image, Button, TouchableOpacity } from 'react-native'
 
 import data from './heroes.json'
 
@@ -16,37 +16,51 @@ class gameBoardScreen extends Component {
     }
 
     componentDidMount(){
-        console.log(this.state.player_one_heroes);
+        // console.log(this.state.player_one_heroes);
     }
 
     componentWillMount(){
-        obj_hero = []
+        obj_hero_one = []
+        obj_hero_two = []
         for(let i = 0 ; i < 4 ; i++){
-            let rand = Math.floor(Math.random() * 19) + 0 ;
-            let hero = data[rand];
-            obj_hero.push(hero)
+            let rand_one = Math.floor(Math.random() * 19) + 0 ;
+            let rand_two = Math.floor(Math.random() * 19) + 0 ;
+            let hero_one = data[rand_one];
+            let hero_two = data[rand_two];
+            obj_hero_one.push(hero_one);
+            obj_hero_two.push(hero_two);
         }
-        console.log(obj_hero)
         this.setState({
-            player_one_heroes: [...this.state.player_one_heroes, obj_hero]
-          })
+            player_one_heroes: [...this.state.player_one_heroes, obj_hero_one]
+        })
+        this.setState({
+            player_two_heroes: [...this.state.player_two_heroes, obj_hero_two]
+        })
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.imagesWrapperPlayerOne}>
-                    <Image style={styles.imagePlayerOne} source={require('../Themes/Images/Heroes/symmetra.png')}/>
-                    <Image style={styles.imagePlayerOne} source={require('../Themes/Images/Heroes/symmetra.png')}/>
-                    <Image style={styles.imagePlayerOne} source={require('../Themes/Images/Heroes/symmetra.png')}/>
-                    <Image style={styles.imagePlayerOne} source={require('../Themes/Images/Heroes/symmetra.png')}/>
+                    {
+                        this.state.player_one_heroes[0].map(function(index, key) {
+                            return(
+                                <TouchableOpacity onPress={() => console.warn(key) }  style={styles.containerOne}  key={{key}}>
+                                    <Image style={styles.imagePlayerOne} source={{uri: index.image}}/>
+                                </TouchableOpacity>
+                        )})
+                    }
                 </View>
 
                 <View style={styles.imagesWrapperPlayerTwo}>
-                    <Image style={styles.imagePlayerTwo} source={require('../Themes/Images/Heroes/symmetra.png')}/>
-                    <Image style={styles.imagePlayerTwo} source={require('../Themes/Images/Heroes/symmetra.png')}/>
-                    <Image style={styles.imagePlayerTwo} source={require('../Themes/Images/Heroes/symmetra.png')}/>
-                    <Image style={styles.imagePlayerTwo} source={require('../Themes/Images/Heroes/symmetra.png')}/>
+                {
+                        this.state.player_two_heroes[0].map(function(index, key) {
+                            return(
+                            <TouchableOpacity style={styles.containerTwo}  key={{key}}>
+                                <Image key={{key}} style={styles.imagePlayerTwo} source={{uri: index.image}}/>
+                            </TouchableOpacity>
+                        )})
+                }
                 </View>
             </View>
         )
@@ -58,16 +72,16 @@ let styles = {
         flex: 1
     },
     imagesWrapperPlayerOne : {
-        flex: 4,
+        flex: 1,
         flexDirection: 'row',
         marginTop: 20,
         alignItems: 'stretch'
     },
     imagePlayerOne: {
-        flex: 1,
-        resizeMode: 'contain',
         transform: [{rotate: '180deg'}], 
-        height: 150
+        height: 150,
+        width:90,
+        resizeMode: 'contain'
     },
     imagesWrapperPlayerTwo : {
         flexDirection: 'row',
@@ -75,9 +89,9 @@ let styles = {
         alignItems: 'stretch'
     },
     imagePlayerTwo: {
-        flex: 1,
-        resizeMode: 'contain',
-        height: 150
+        height: 150,
+        width:90,
+        resizeMode: 'contain'
     }
 }
 
