b1b2cb59ce763872ced95e7d0aa4bb9033e3bf12
Sahra Vadrot <apexp@hotmail.fr>
2018-12-14 21:13:28 +0100

    :sparkles: Fetch get on api overwatch
    api overwatch #10

--- App/Containers/ProfilOverwatchScreen.js
+++ App/Containers/ProfilOverwatchScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, Text, StyleSheet, Image, ImageBackground, FlatList} from 'react-native'
+import { View, Text, StyleSheet, TextInput, Picker, Button } from 'react-native'
 
 class ProfilOverwatchScreen extends Component {
 
@@ -8,15 +8,71 @@ class ProfilOverwatchScreen extends Component {
         super();
 
         this.state = {
+            plateform: "pc",
+            region: "eu",
+            battletag: ""
         };
 
     }
 
+    getDataParameters() {
+        let url = "https://ow-api.com/v1/stats/" + this.state.plateform + "/" + this.state.region + "/" + this.state.battletag + "/profile";
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = (e) => {
+          if (request.readyState !== 4) {
+            return;
+          }
+        
+          if (request.status === 200) {
+            console.warn('success', request.responseText);
+          } else {
+            console.warn('error');
+          }
+        };
+        
+        request.open('GET', url);
+        request.send();
+    }
 
-    render () {
+
+    render() {
         return (
             <View>
-                <Text>Han han</Text>
+                <View>
+                    <Text>Platform</Text>
+                    <Picker
+                        selectedValue={this.state.plateform}
+                        style={{ height: 50, width: 100 }}
+                        onValueChange={(itemValue) => this.setState({ plateform: itemValue })}>
+                        <Picker.Item label="PC" value="pc" />
+                        <Picker.Item label="Xbox" value="xbox" />
+                        <Picker.Item label="Playstation 4" value="ps4" />
+                    </Picker>
+                </View>
+                <View>
+                    <Text>Region</Text>
+                    <Picker
+                        selectedValue={this.state.region}
+                        style={{ height: 50, width: 100 }}
+                        onValueChange={(itemValue) => this.setState({ region: itemValue })}>
+                        <Picker.Item label="Europe" value="eu" />
+                        <Picker.Item label="Asia" value="asia" />
+                        <Picker.Item label="America" value="us" />
+                    </Picker>
+                </View>
+                <View>
+                    <Text>Battletag</Text>
+                    <TextInput
+                        onChangeText={(text) => this.setState({ battletag: text })}
+                        value={this.state.battletag}
+                    />
+                </View>
+                <View>
+                    <Button
+                        onPress={() => this.getDataParameters()}
+                        title="Search player"
+                    />
+                </View>
             </View>
         )
     }
