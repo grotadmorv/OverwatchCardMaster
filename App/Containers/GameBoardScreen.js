49ede082af774c13534443728a62ebc0bb42b0aa
Sahra Vadrot <apexp@hotmail.fr>
2018-12-16 22:19:40 +0100

    :construction: LocalStorage done, profil in progress
    LS done, profil in progress #10

--- App/Containers/GameBoardScreen.js
+++ App/Containers/GameBoardScreen.js
@@ -1,32 +1,48 @@
 import React, { Component } from 'react';
 
-import { View, Text, Image, Button, TouchableOpacity, AsyncStorage  } from 'react-native'
+import { View, Text, Image, Button, TouchableOpacity, AsyncStorage } from 'react-native';
 
-import data from './heroes.json'
+import Storage from 'react-native-storage';
+
+import data from './heroes.json';
+
+const storage = new Storage({
+    size: 2000,
+
+    storageBackend: AsyncStorage,
+
+    defaultExpires: null,
+
+    enableCache: true,
+
+});
+// global.storage = storage;
+
 
 class gameBoardScreen extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             player_one_heroes: [],
             player_two_heroes: [],
-            player_one: {hp : 50},
-            player_two: {hp: 50},
-            turn: 1
+            player_one: { hp: 50 },
+            player_two: { hp: 50 },
+            turn: 1,
+            all_hero: [{name: "tracer", win: 0},{name: "winston", win: 0},{name: "pharah", win: 0},{name: "reinhardt", win: 0},{name: "symmetra", win: 0},{name: "reaper", win: 0},{name: "mccree", win: 0},{name: "mercy", win: 0},{name: "mei", win: 0},{name: "roadhog", win: 0},{name: "junkrat", win: 0},{name: "hanzo", win: 0},{name: "window", win: 0},{name: "zenyatta", win: 0},{name: "bastion", win: 0},{name: "zarya", win: 0},{name: "lucio", win: 0},{name: "dva", win: 0},{name: "soldier_76", win: 0},{name: "genji", win: 0}]
         }
     }
 
-    componentDidMount(){
-        // console.warn(this.state.player_one.hp);
+    componentDidMount() {
+        // console.warn(this.state.player_one_heroes);
     }
 
-    componentWillMount(){
+    componentWillMount() {
         obj_hero_one = []
         obj_hero_two = []
-        for(let i = 0 ; i < 4 ; i++){
-            let rand_one = Math.floor(Math.random() * 19) + 0 ;
-            let rand_two = Math.floor(Math.random() * 19) + 0 ;
+        for (let i = 0; i < 4; i++) {
+            let rand_one = Math.floor(Math.random() * 19) + 0;
+            let rand_two = Math.floor(Math.random() * 19) + 0;
             let hero_one = data[rand_one];
             let hero_two = data[rand_two];
             obj_hero_one.push(hero_one);
@@ -40,45 +56,89 @@ class gameBoardScreen extends Component {
         })
     }
 
-    onAttack(turn, index){
-        if(this.state.turn == turn){
+    onAttack(turn, index) {
+        if (this.state.turn == turn) {
             rand = Math.floor(Math.random() * 10) + 1;
-            if(index.critical_hit - 1 >= rand ){
-                critical = 1 + (index.critical_hit/100);
+            if (index.critical_hit - 1 >= rand) {
+                critical = 1 + (index.critical_hit / 100);
                 attack = Math.ceil(index.attack * critical)
 
                 // todo animation hit
-            }else{
+            } else {
                 attack = index.attack
             }
 
             // todo animation attack
-            if(this.state.turn == 1){
-                if( this.state.player_two.hp - attack <= 0 ){
+            if (this.state.turn == 1) {
+                if (this.state.player_two.hp - attack <= 0) {
                     hp = 0
                     this.onWin(1)
-                }else{
+                } else {
                     hp = this.state.player_two.hp - attack
                 }
-                this.setState({player_two: {hp: hp},  turn: 2})
-            }else{
-                if(this.state.player_one.hp - attack <= 0){
+                this.setState({ player_two: { hp: hp }, turn: 2 })
+            } else {
+                if (this.state.player_one.hp - attack <= 0) {
                     hp = 0
                     this.onWin(2)
-                }else{
+                } else {
                     hp = this.state.player_one.hp - attack
                 }
-                this.setState({player_one: {hp: (this.state.player_one.hp - attack)},  turn: 1}) 
+                this.setState({ player_one: { hp: (this.state.player_one.hp - attack) }, turn: 1 })
             }
         }
     }
 
-    onWin(winner){
+
+    loadPlayerData(winner) {
+        storage
+            .load({
+                key: 'dataPlayer',
+                autoSync: true,
+                syncInBackground: true,
+            })
+            .then(ret => {
+                let total_won_played = ret.gameWon;
+                let total_game_played = ret.gamePlayed += 1
+                
+                if(winner){
+                    total_won_played += 1
+                }
+
+                storage.save({
+                    key: 'dataPlayer',
+                    data: {
+                        gamePlayed: total_game_played,
+                        gameWon: total_won_played,
+                        gameLastHeroes: this.state.player_one_heroes[0]
+                    },
+
+                    expires: null
+                });
+            })
+            .catch(err => {
+                let winnerNum = 0
+                if (winner){
+                    winnerNum = 1;
+                }
+                if (err.name == 'NotFoundError') {
+                    storage.save({
+                        key: 'dataPlayer',
+                        data: {
+                            gamePlayed: 1,
+                            gameWon: winnerNum,
+                            gameLastHeroes: this.state.player_one_heroes[0]
+                        },
+
+                        expires: null
+                    })
+                }
+            });
+    }
+
+    onWin(winner) {
         // todo animation win
-        AsyncStorage.removeItem('gameWon');
-        AsyncStorage.removeItem('gamePlayed');
-        if(winner == 1){
-        }
+        winner == 1 ? this.loadPlayerData(true) : this.loadPlayerData(false);
     }
 
     render() {
@@ -88,27 +148,29 @@ class gameBoardScreen extends Component {
                     {
                         this.state.player_one_heroes[0].map((index, key) => {
 
-                            return(
-                                <TouchableOpacity onPress={() => this.onAttack(1,index) }  style={styles.containerOne}  key={{key}}>
-                                    <Image style={styles.imagePlayerOne} source={{uri: index.image}}/>
+                            return (
+                                <TouchableOpacity onPress={() => this.onAttack(1, index)} style={styles.containerOne} key={{ key }}>
+                                    <Image style={styles.imagePlayerOne} source={{ uri: index.image }} />
                                 </TouchableOpacity>
-                        )})
+                            )
+                        })
                     }
                 </View>
-                    <View>
-                        <Text style={{marginLeft:100}} >
-                            {this.state.turn} {this.state.player_one.hp} {this.state.player_two.hp}
-                        </Text>
-                    </View>
+                <View>
+                    <Text style={{ marginLeft: 100 }} >
+                        {this.state.turn} {this.state.player_one.hp} {this.state.player_two.hp}
+                    </Text>
+                </View>
                 <View style={styles.imagesWrapperPlayerTwo}>
-                {
+                    {
                         this.state.player_two_heroes[0].map((index, key) => {
-                            return(
-                            <TouchableOpacity onPress={() => this.onAttack(2,index)} style={styles.containerTwo}  key={{key}}>
-                                <Image key={{key}} style={styles.imagePlayerTwo} source={{uri: index.image}}/>
-                            </TouchableOpacity>
-                        )})
-                }
+                            return (
+                                <TouchableOpacity onPress={() => this.onAttack(2, index)} style={styles.containerTwo} key={{ key }}>
+                                    <Image key={{ key }} style={styles.imagePlayerTwo} source={{ uri: index.image }} />
+                                </TouchableOpacity>
+                            )
+                        })
+                    }
                 </View>
             </View>
         )
@@ -119,26 +181,26 @@ let styles = {
     container: {
         flex: 1
     },
-    imagesWrapperPlayerOne : {
+    imagesWrapperPlayerOne: {
         flex: 1,
         flexDirection: 'row',
         marginTop: 20,
         alignItems: 'stretch'
     },
     imagePlayerOne: {
-        transform: [{rotate: '180deg'}], 
+        transform: [{ rotate: '180deg' }],
         height: 150,
-        width:90,
+        width: 90,
         resizeMode: 'contain'
     },
-    imagesWrapperPlayerTwo : {
+    imagesWrapperPlayerTwo: {
         flexDirection: 'row',
         marginBottom: 20,
         alignItems: 'stretch'
     },
     imagePlayerTwo: {
         height: 150,
-        width:90,
+        width: 90,
         resizeMode: 'contain'
     }
 }
